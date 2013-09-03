e9c036b67701fdb648a5e2b3c2adcd15be23390c
zdsiyan <zdsiyan@gmail.com>
2013-09-03 16:37:49 +0800

    关注页和粉丝页

--- src/main/java/com/mgs/watermelon/controller/TwiboController.java
+++ src/main/java/com/mgs/watermelon/controller/TwiboController.java
@@ -9,11 +9,14 @@ import org.apache.log4j.Logger;
 import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.mgs.watermelon.entity.MUser;
 import com.mgs.watermelon.entity.Twibo;
+import com.mgs.watermelon.service.MUserService;
 import com.mgs.watermelon.service.TwiboService;
 import com.mgs.watermelon.vo.ResultVO;
 import com.mgs.watermelon.vo.SysDefinition;
@@ -28,6 +31,9 @@ public class TwiboController {
 	
 	@Autowired
 	private TwiboService twiboService;
+	
+	@Autowired
+	private MUserService mUserService;
 	
 	/**
 	 * 发微博
@@ -78,6 +84,46 @@ public class TwiboController {
 		}else{
 			return new ResultVO<List<Twibo>>(SysDefinition.CODE_NODATA,null,null);
 		}
+	}
+	
+	/**
+	 * 查询所有关注者
+	 * @param session
+	 * @param oid
+	 * @return
+	 */
+	@RequestMapping(value="/{oid}/follow")
+	public ModelAndView follow(HttpSession session, @PathVariable String oid){
+		ModelAndView view = new ModelAndView("/follow");
+		try{
+			MUser muser= mUserService.get(new ObjectId(oid));
+			if(muser!=null){
+				view.addObject("follows",muser.getFollows());
+			}
+		}catch(Exception e){
+			logger.error(e.getMessage());
+		}
+		return view;
+	}
+	
+	/**
+	 * 查询所有粉丝
+	 * @param session
+	 * @param oid
+	 * @return
+	 */
+	@RequestMapping(value="/{oid}/fans")
+	public ModelAndView fans(HttpSession session, @PathVariable String oid){
+		ModelAndView view = new ModelAndView("/fans");
+		try{
+			MUser muser= mUserService.get(new ObjectId(oid));
+			if(muser!=null){
+				view.addObject("fans",muser.getFans());
+			}
+		}catch(Exception e){
+			logger.error(e.getMessage());
+		}
+		return view;
 	}
 	
 	@ResponseBody
